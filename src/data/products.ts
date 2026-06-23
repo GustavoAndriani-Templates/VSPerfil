d6855fb90f980eac6c72fa7f31505c6b6fb6ae02
Gustavo Andriani <gustavo.joseandriani@gmail.com>
2026-06-23 11:44:51 -0300

    fix: Add new images to products

--- src/data/products.ts
+++ src/data/products.ts
@@ -8,13 +8,14 @@ export const productsData: Products[] = [
     category: 'quadrado',
     shortDescription: 'Tubo quadrado de aço galvanizado, ideal para estruturas metálicas e projetos de construção civil',
     fullDescription: 'O tubo quadrado de aço galvanizado é fabricado com aço de alta qualidade e revestido com uma camada de zinco para proteção contra corrosão. É perfeito para aplicações que exigem resistência e durabilidade, como estruturas metálicas, suportes, grades e projetos de construção civil. Sua superfície galvanizada garante longa vida útil mesmo em ambientes externos, tornando-o uma escolha confiável para diversos tipos de projetos.',
-    image: '/productsImages/tubo_quadrado_15x15/tubo_quadrado_15x15__01.jpg',
+    image: '/productsImages/tubo_quadrado/tubo_quadrado.jpg',
     gallery: [
-      '/productsImages/tubo_quadrado_15x15/tubo_quadrado_15x15__02.jpg',
-      '/productsImages/tubo_quadrado_15x15/tubo_quadrado_15x15__03.jpg',
+      '/productsImages/tubo_quadrado/tubo_quadrado_3.jpg',
+      '/productsImages/tubo_quadrado/tubo_quadrado_5.png',
+      '/productsImages/tubo_quadrado/tubo_quadrado_4.png',
     ],
     technicalDetails: {
-      'Material': 'Fina Frio | Aço Carbono',
+      'Material': 'Fina Frio ou Aço Carbono',
       'Composição do Feixe': '256 barras',
     }
   },
@@ -41,14 +42,15 @@ export const productsData: Products[] = [
     category: 'redondo',
     shortDescription: 'Tubo redondo de aço galvanizado, ideal para aplicações estruturais e projetos de construção civil',
     fullDescription: 'O tubo redondo de aço galvanizado é fabricado com aço de alta qualidade e revestido com uma camada de zinco para proteção contra corrosão. É perfeito para aplicações que exigem resistência e durabilidade, como estruturas metálicas, suportes, grades e projetos de construção civil. Sua superfície galvanizada garante longa vida útil mesmo em ambientes externos, tornando-o uma escolha confiável para diversos tipos de projetos.',
-    image: '/productsImages/tubo_redondo_5-8/tubo_redondo_5-8.jpeg',
+    image: '/productsImages/tubo_redondo/tubo_redondo.jpg',
     gallery: [
-      '/productsImages/tubo_redondo_5-8/tubo_redondo_5-8__01.jpg',
-      '/productsImages/tubo_redondo_5-8/tubo_redondo_5-8__02.jpg',
-      '/productsImages/tubo_redondo_5-8/tubo_redondo_5-8__03.jpg',
+      '/productsImages/tubo_redondo/tubo_redondo_3.jpg',
+      // '/productsImages/tubo_redondo/tubo_redondo_4.jpg',
+      '/productsImages/tubo_redondo/tubo_redondo_5.jpg',
+      '/productsImages/tubo_redondo/tubo_redondo_7.jpg',
     ],
     technicalDetails: {
-      'Material': 'Fina Frio | Aço Carbono',
+      'Material': 'Fina Frio ou Aço Carbono',
       'Composição do Amarrado': '397 barras',
     }
   },
@@ -75,13 +77,14 @@ export const productsData: Products[] = [
     category: 'retangular',
     shortDescription: 'Tubo retangular de aço galvanizado, ideal para estruturas metálicas e projetos de construção civil',
     fullDescription: 'O tubo retangular de aço galvanizado é fabricado com aço de alta qualidade e revestido com uma camada de zinco para proteção contra corrosão. É perfeito para aplicações que exigem resistência e durabilidade, como estruturas metálicas, suportes, grades e projetos de construção civil. Sua superfície galvanizada garante longa vida útil mesmo em ambientes externos, tornando-o uma escolha confiável para diversos tipos de projetos.',
-    image: '/productsImages/tubo_retangular_30x20/tubo_retangular_30x20__03.jpg',
+    image: '/productsImages/tubo_retangular/tubo_retangular.png',
     gallery: [
-      '/productsImages/tubo_retangular_30x20/tubo_retangular_30x20__01.jpg',
-      '/productsImages/tubo_retangular_30x20/tubo_retangular_30x20__02.jpg',
+      '/productsImages/tubo_retangular/tubo_retangular_2.png',
+      '/productsImages/tubo_retangular/tubo_retangular_3.png',
+      '/productsImages/tubo_retangular/tubo_retangular_4.png',
     ],
     technicalDetails: {
-      'Material': 'Fina Frio | Aço Carbono',
+      'Material': 'Fina Frio ou Aço Carbono',
       'Composição do Feixe': '224 barras',
     }
   },
